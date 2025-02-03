46a84df40ce5e9d9131234eaea52a9dd8b4623bf
Pengjie He <hepengjie789@hotmail.com>
2025-02-04 02:33:37 +0800

    add initial lastupdate table data

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,6 +1,9 @@
 from sqlmodel import Session, create_engine
 from app.core.config import settings
-from app.models import Transaction, LastUpdate, SpotPrice
+from app.models import LastUpdateCreate
+from app import crud
+
+from datetime import datetime, timedelta, timezone
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
@@ -16,4 +19,14 @@ def init_db(session: Session) -> None:
 
     # This works because the models are already imported and registered from app.models
     # SQLModel.metadata.create_all(engine)
-    return
+    init_lastupdate_timestamp = datetime.now(timezone.utc) - timedelta(minutes=5)
+
+    lastupdate_transaction = crud.get_lastupdate_transaction(session=session)
+    if not lastupdate_transaction:
+        lastupdate_transaction = LastUpdateCreate(type="transaction", timestamp=init_lastupdate_timestamp)
+        crud.insert_lastupdate_transaction(session=session, lastupdate_transaction_insert=lastupdate_transaction)
+
+    lastupdate_spot_price = crud.get_lastupdate_spot_price(session=session)
+    if not lastupdate_spot_price:
+        lastupdate_spot_price = LastUpdateCreate(type="spot_price", timestamp=init_lastupdate_timestamp)
+        crud.insert_lastupdate_spot_price(session=session, lastupdate_spot_price_insert=lastupdate_spot_price)
